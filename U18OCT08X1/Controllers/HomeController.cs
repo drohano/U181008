8c68cae6d5b8001475e7990fcb5df664834fb5dc
Drohano <joix99@live.se>
2018-10-08 13:32:20 +0200

    created the basic structure added bootstrap

---
+++ U18OCT08X1/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace U18OCT08X1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult About()
+        {
+            return View();
+        }
+
+        public ViewResult Contact()
+        {
+            return View();
+        }
+     
+    }
+}
